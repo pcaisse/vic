45ca3298fc12ce19ffc4e9ecc1c4fd81925d9d8b
Peter Caisse <pcaisse@gmail.com>
2024-05-05 10:38:15 -0400

    Add crossterm and capture and print user input

--- main.rs
+++ main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::io::{Write, stdout};
+use crossterm::style::Print;
+use crossterm::QueueableCommand;
+
+fn main() -> io::Result<()> {
+    loop {
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let mut stdout = stdout();
+        stdout.queue(Print(input))?;
+        stdout.flush()?;
+    }
 }
