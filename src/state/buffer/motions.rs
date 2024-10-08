1985be9463f0a858b5de3029fc3c26abf3065f1c
Peter Caisse <pcaisse@gmail.com>
2024-10-08 11:37:09 -0400

    Fix bigword backwards motion

--- src/state/buffer/motions.rs
+++ src/state/buffer/motions.rs
@@ -17,32 +17,82 @@ fn grapheme_iter(s: &str, i: usize, is_forwards: bool) -> impl Iterator<Item = (
     }
 }
 
-fn big_word(s: &str, i: usize, is_forwards: bool) -> usize {
-    let iter = grapheme_iter(s, i, is_forwards);
+// fn next_bigword(iter: impl Iterator<Item = (usize, &str)>) {
+//     iter.tuple_windows()
+//         .take_while_inclusive(|((_, curr_grapheme), (_, next_grapheme))| {
+//             // Stop when the current grapheme is blank and the next grapheme isn't blank
+//             !is_whitespace_str(curr_grapheme) || is_whitespace_str(next_grapheme)
+//         })
+//         .unzip()
+// }
+
+// "hi  there"
+// (0, "i") (2, " ")
+// (1, "i") (2, " ")
+// (2, " ") (3, " ")
+// (3, " ") (4, "t")
+
+pub fn big_word_forwards(s: &str, i: usize) -> usize {
+    let iter = grapheme_iter(s, i, true);
     // Find next bigword
     let (_current, next): (Vec<_>, Vec<_>) = iter
         .tuple_windows()
         .take_while_inclusive(|((_, curr_grapheme), (_, next_grapheme))| {
+            // Stop when the current grapheme is blank and the next grapheme isn't blank
             !is_whitespace_str(curr_grapheme) || is_whitespace_str(next_grapheme)
         })
         .unzip();
     // Extract grapheme indices
     let (indices, _graphemes): (Vec<usize>, Vec<&str>) = next.into_iter().unzip();
-    if is_forwards {
-        // Return last grapheme index offset by cursor position
-        let index = indices.into_iter().max().unwrap_or_default();
-        i + index
-    } else {
-        // Return first grapheme index offset by cursor position
-        let index = indices.into_iter().min().unwrap_or_default();
-        i - (i - index)
-    }
+    // Return last grapheme index offset by cursor position
+    let index = indices.into_iter().max().unwrap_or_default();
+    i + index
 }
 
-pub fn big_word_forwards(s: &str, i: usize) -> usize {
-    big_word(s, i, true)
-}
+// "hi  there"
+// (0, "h") (1, "t")
+// (1, "t") (2, " ")
+// (2, " ") (3, " ")
+// (3, " ") (4, "i")
 
 pub fn big_word_backwards(s: &str, i: usize) -> usize {
-    big_word(s, i, false)
+    let s_end_trimmed = s.trim_end();
+    let mut iter = grapheme_iter(s_end_trimmed, i, false).peekable();
+    let p = iter.peek();
+    let result = match (s != s_end_trimmed, p) {
+        (true, _) => {
+            // We started on whitespace, so go to beginning of next bigword
+            iter.take_while(|(_, grapheme)| !is_whitespace_str(grapheme))
+                .collect()
+        }
+        (_, Some((_, next_grapheme))) if !is_whitespace_str(next_grapheme) => {
+            // We started *not* on whitespace and the next grapheme is also not whitespace, such as
+            // in the middle of a bigword, so go to end of the next bigword
+            let (current, _next): (Vec<_>, Vec<_>) = iter
+                .tuple_windows()
+                .take_while_inclusive(|(_, (_, next_grapheme))| {
+                    // Stop when the next grapheme is blank
+                    !is_whitespace_str(next_grapheme)
+                })
+                .unzip();
+            current
+        }
+        _ => {
+            // We're at the beginning of a bigword, so keep going until we get to the end of
+            // another bigword
+            let (_current, next): (Vec<_>, Vec<_>) = iter
+                .tuple_windows()
+                .take_while_inclusive(|((_, curr_grapheme), (_, next_grapheme))| {
+                    // Stop when the current grapheme is blank and the next grapheme isn't blank
+                    !is_whitespace_str(curr_grapheme) || is_whitespace_str(next_grapheme)
+                })
+                .unzip();
+            next
+        }
+    };
+    // Extract grapheme indices
+    let (indices, _graphemes): (Vec<usize>, Vec<&str>) = result.into_iter().unzip();
+    // Return first grapheme index offset by cursor position
+    let index = indices.into_iter().min().unwrap_or_default();
+    i - (i - index)
 }
--- tests/motions.rs
+++ tests/motions.rs
@@ -6,6 +6,7 @@ fn test_big_word_forwards() {
     assert_eq!(big_word_forwards("hi", 1), 1);
     assert_eq!(big_word_forwards("hi\n", 0), 2);
     assert_eq!(big_word_forwards("hi   there", 1), 5);
+    assert_eq!(big_word_forwards("hey   there", 1), 6);
     assert_eq!(big_word_forwards("", 0), 0);
     assert_eq!(big_word_forwards("नमस्ते foo bar", 0), 19);
 }
@@ -15,8 +16,10 @@ fn test_big_word_backwards() {
     assert_eq!(big_word_backwards("hi", 0), 0);
     assert_eq!(big_word_backwards("a b c", 4), 2);
     assert_eq!(big_word_backwards("a b c", 2), 0);
-    assert_eq!(big_word_backwards("hi\n", 2), 0);
+    assert_eq!(big_word_backwards("hello ", 5), 0);
     assert_eq!(big_word_backwards("hi   there", 5), 1);
+    assert_eq!(big_word_backwards("hi   there", 6), 5);
+    assert_eq!(big_word_backwards("hi   there", 9), 5);
     assert_eq!(big_word_backwards("", 0), 0);
     assert_eq!(big_word_backwards("नमस्ते foo bar", 19), 0);
 }
